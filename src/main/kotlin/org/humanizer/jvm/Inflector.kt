81465abac7cfec10c0b4bcf4c29810b64e430523
Christiaan baes <chris.baes@gmail.com>
2014-05-07 10:17:52 +0200

    pluralize and singularize

--- src/main/kotlin/org/humanizer/jvm/Inflector.kt
+++ src/main/kotlin/org/humanizer/jvm/Inflector.kt
@@ -1,79 +1,192 @@
 package org.humanizer.jvm
 
-fun String.camelize() : String{
+import java.util.regex.Pattern
+
+fun String.camelize(): String {
     var previousWasUnderscore = false
     var t = ""
     this.decapitalize().forEach {
         if (it.toString().equals("_")) previousWasUnderscore = true
-        if(Character.isLetterOrDigit(it) && previousWasUnderscore) {
+        if (Character.isLetterOrDigit(it) && previousWasUnderscore) {
             previousWasUnderscore = false
             t = "${t}${it.toString().toUpperCase()}"
-        }
-        else
-        {
+        } else {
             t = "${t}${it}"
         }
     }
-    return t.replace("_","")
+    return t.replace("_", "")
 }
 
-fun String.pascalize() : String{
+fun String.pascalize(): String {
     var previousWasUnderscore = false
     var t = ""
     this.capitalize().forEach {
         if (it.toString().equals("_")) previousWasUnderscore = true
-        if(Character.isLetterOrDigit(it) && previousWasUnderscore) {
+        if (Character.isLetterOrDigit(it) && previousWasUnderscore) {
             previousWasUnderscore = false
             t = "${t}${it.toString().toUpperCase()}"
-        }
-        else
-        {
+        } else {
             t = "${t}${it}"
         }
     }
-    return t.replace("_","")
+    return t.replace("_", "")
 }
 
-fun String.underscore() : String{
+fun String.underscore(): String {
     var previousWasCapital = false
     var t = ""
     this.decapitalize().forEach {
         if (Character.isUpperCase(it)) previousWasCapital = true
-        if(Character.isLetterOrDigit(it) && previousWasCapital) {
+        if (Character.isLetterOrDigit(it) && previousWasCapital) {
             previousWasCapital = false
             t = "${t}_${it.toString().toLowerCase()}"
-        }
-        else
-        {
+        } else {
             t = "${t}${it}"
         }
     }
-    return t.replace(" ","_")
+    return t.replace(" ", "_")
 }
 
-fun String.titleize() : String{
+fun String.titleize(): String {
     var previousWasWhitespace = false
     var t = ""
-    this.replace("-"," ").replace("_", " ").capitalize()
+    this.replace("-", " ").replace("_", " ").capitalize()
             .forEach {
         if (Character.isWhitespace(it) || !Character.isLetterOrDigit(it)) previousWasWhitespace = true
-        if(Character.isLetterOrDigit(it) && previousWasWhitespace) {
+        if (Character.isLetterOrDigit(it) && previousWasWhitespace) {
             previousWasWhitespace = false
             t = "${t}${it.toString().toUpperCase()}"
-        }
-        else
-        {
+        } else {
             t = "${t}${it}"
         }
     }
     return t
 }
 
-fun String.dasherize() : String{
+fun String.dasherize(): String {
     return this.replace("_", "-")
 }
 
-fun String.hyphenate() : String{
+fun String.hyphenate(): String {
     return this.replace("_", "-")
 }
 
+/*
+The pluralize and singularize methods are based on the code found in the following places.
+
+https://github.com/atteo/evo-inflector/blob/master/src/main/java/org/atteo/evo/inflector/English.java
+http://www.java2s.com/Tutorial/Java/0040__Data-Type/Transformswordstosingularpluralhumanizedhumanreadableunderscorecamelcaseorordinalform.htm
+https://github.com/rails/rails/blob/26698fb91d88dca0f860adcb80528d8d3f0f6285/activesupport/lib/active_support/inflector/inflections.rb
+
+ */
+fun String.pluralize(): String {
+    if (unCountable().contains(this)) return this
+    val rule = pluralizeRules().last { Pattern.compile(it.component1(), Pattern.CASE_INSENSITIVE).matcher(this).find() }
+    var found = Pattern.compile(rule.component1(), Pattern.CASE_INSENSITIVE).matcher(this).replaceAll(rule.component2())
+    val excep = exceptions().firstOrNull() { this.equals(it.component1()) }
+    if (excep != null) found = excep.component2()
+    val endswith = exceptions().firstOrNull { this.endsWith(it.component1()) }
+    if (endswith != null) found = this.replace(endswith.component1(), endswith.component2())
+    return found
+}
+
+fun String.singularize(): String {
+    if (unCountable().contains(this)) return this
+    val excep = exceptions().firstOrNull() { this.equals(it.component2()) }
+    if (excep != null) return excep.component1()
+    val endswith = exceptions().firstOrNull { this.endsWith(it.component2()) }
+    if (endswith != null) return this.replace(endswith.component2(), endswith.component1())
+    val rule = singularizeRules().last { Pattern.compile(it.component1(), Pattern.CASE_INSENSITIVE).matcher(this).find() }
+    return Pattern.compile(rule.component1(), Pattern.CASE_INSENSITIVE).matcher(this).replaceAll(rule.component2())
+}
+fun unCountable(): List<String> {
+    return listOf("equipment", "information", "rice", "money",
+            "species", "series", "fish", "sheep", "aircraft", "bison", "flounder", "pliers", "bream",
+            "gallows", "proceedings", "breeches", "graffiti", "rabies",
+            "britches", "headquarters", "salmon", "carp", "herpes",
+            "scissors", "chassis", "high-jinks", "sea-bass", "clippers",
+            "homework", "cod", "innings", "shears",
+            "contretemps", "jackanapes", "corps", "mackerel",
+            "swine", "debris", "measles", "trout", "diabetes", "mews",
+            "tuna", "djinn", "mumps", "whiting", "eland", "news",
+            "wildebeest", "elk", "pincers", "sugar")
+}
+
+fun exceptions(): List<Pair<String, String>> {
+    return listOf("person" to "people",
+            "man" to "men",
+            "goose" to "geese",
+            "child" to "children",
+            "sex" to "sexes",
+            "move" to "moves",
+            "stadium" to "stadiums",
+            "deer" to "deer",
+            "codex" to "codices",
+            "murex" to "murices",
+            "silex" to "silices",
+            "radix" to "radices",
+            "helix" to "helices")
+}
+
+fun pluralizeRules(): List<Pair<String, String>> {
+    return listOf(
+            "$" to "s",
+            "s$"to "s",
+            "(ax|test)is$" to "$1es",
+            "us$" to "i",
+            "(octop|vir)us$" to "$1i",
+            "(octop|vir)i$" to "$1i",
+            "(alias|status)$" to "$1es",
+            "(bu)s$" to "$1ses",
+            "(buffal|tomat)o$" to "$1oes",
+            "([ti])um$" to "$1a",
+            "([ti])a$" to "$1a",
+            "sis$" to "ses",
+            "(,:([^f])fe|([lr])f)$" to "$1$2ves",
+            "(hive)$" to "$1s",
+            "([^aeiouy]|qu)y$" to "$1ies",
+            "(x|ch|ss|sh)$" to "$1es",
+            "(matr|vert|ind)ix|ex$" to "$1ices",
+            "([m|l])ouse$" to "$1ice",
+            "([m|l])ice$" to "$1ice",
+            "^(ox)$" to "$1en",
+            "(quiz)$" to "$1zes",
+            "f$" to "ves",
+            "fe$" to "ves",
+            "um$" to "a")
+}
+
+fun singularizeRules(): List<Pair<String, String>> {
+    return listOf(
+            "s$" to "",
+            "(s|si|u)s$" to "$1s",
+            "(n)ews$" to "$1ews",
+            "([ti])a$" to "$1um",
+            "((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)ses$" to "$1$2sis",
+            "(^analy)ses$" to "$1sis",
+            "(^analy)sis$" to "$1sis",
+            "([^f])ves$" to "$1fe",
+            "(hive)s$" to "$1",
+            "(tive)s$" to "$1",
+            "([lr])ves$" to "$1f",
+            "([^aeiouy]|qu)ies$" to "$1y",
+            "(s)eries$" to "$1eries",
+            "(m)ovies$" to "$1ovie",
+            "(x|ch|ss|sh)es$" to "$1",
+            "([m|l])ice$" to "$1ouse",
+            "(bus)es$" to "$1",
+            "(o)es$" to "$1",
+            "(shoe)s$" to "$1",
+            "(cris|ax|test)is$" to "$1is",
+            "(cris|ax|test)es$" to "$1is",
+            "(octop|vir)i$" to "$1us",
+            "(octop|vir)us$" to "$1us",
+            "(alias|status)es$" to "$1",
+            "(alias|status)$" to "$1",
+            "^(ox)en" to "$1",
+            "(vert|ind)ices$" to "$1ex",
+            "(matr)ices$" to "$1ix",
+            "(quiz)zes$" to "$1",
+            "a$" to "um",
+            "i$" to "us")
+}
